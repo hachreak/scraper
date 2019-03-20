88bee596cd2e6a90cb73e1ca14dbc2235832e91b
Leonardo Rossi <leonardo.rossi@unipr.it>
2019-03-20 18:15:53 +0100

    utils: raise exception if error persist
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

--- scraper/utils.py
+++ scraper/utils.py
@@ -4,6 +4,8 @@
 from selenium.common.exceptions import StaleElementReferenceException
 from time import sleep
 
+from .exc import UnknowObject
+
 
 def try_again(fun, exc=StaleElementReferenceException, times=5, sleep_time=1):
     for _ in range(times):
@@ -11,3 +13,5 @@ def try_again(fun, exc=StaleElementReferenceException, times=5, sleep_time=1):
             return fun()
         except exc:
             sleep(sleep_time)
+
+    raise UnknowObject()
